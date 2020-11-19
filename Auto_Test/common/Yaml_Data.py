401b0a85851065c96e7f3714bd42472cc76fcaee
unknown <shaojunshuai@chengtay.com>
2020-11-19 17:25:49 +0800

    'add'

--- Auto_Test/common/Yaml_Data.py
+++ Auto_Test/common/Yaml_Data.py
@@ -18,8 +18,8 @@ class HandleYaml:
             # os.path.abspath('.')表示获取当前文件所在目录；os.path.dirname表示获取文件所在父目录；所以整个就是项目的所在路径
             print(root_dir)
             if platform.system() != 'Windows':
-                # 远程Jenkins路径：
-                self.file_path = root_dir + '/Auto_Test/test_data/test_yaml_data.yaml'
+                # linux路径：
+                self.file_path = root_dir + '\\Auto_Test\\test_data\\test_yaml_data.yaml'
             else:
                 self.file_path = root_dir + '\\Auto_Test\\test_data\\test_yaml_data.yaml'  # 获取文件所在的相对路径(相对整个项目)
 
