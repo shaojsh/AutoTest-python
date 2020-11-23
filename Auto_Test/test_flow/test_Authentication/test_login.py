dbfb631e1e745f7d53af5796a647431fbec1ab25
unknown <shaojunshuai@chengtay.com>
2020-11-23 13:42:21 +0800

    'add'

--- Auto_Test/test_flow/test_Authentication/test_login.py
+++ Auto_Test/test_flow/test_Authentication/test_login.py
@@ -57,11 +57,13 @@ def test_companyRegister():
         if jenkins:
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+            chrome_options.add_argument()
             chrome_options.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
             chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
             chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
             chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
             chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument('--disable-useAutomationExtension')
             driver = webdriver.Chrome(executable_path=driverPath, chrome_options=chrome_options)
         else:
             driver = webdriver.Chrome(executable_path=driverPath)
