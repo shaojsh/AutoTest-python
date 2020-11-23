6abccbc0dc91074a406af872b236d8f0598e7bbf
unknown <shaojunshuai@chengtay.com>
2020-11-23 08:59:40 +0800

    'add'\

--- Auto_Test/test_flow/test_Authentication/test_login.py
+++ Auto_Test/test_flow/test_Authentication/test_login.py
@@ -57,7 +57,7 @@ def test_companyRegister():
 
         if jenkins:
             chrome_option = webdriver.ChromeOptions()
-            chrome_option.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
+            # chrome_option.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
             chrome_option.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
             chrome_option.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
             chrome_option.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
