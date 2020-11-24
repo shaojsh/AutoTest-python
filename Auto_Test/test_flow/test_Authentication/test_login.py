21339ba4b2637c2b4df8e84ca8f7dd8df1cfe8bc
unknown <shaojunshuai@chengtay.com>
2020-11-24 17:13:31 +0800

    'add'

--- Auto_Test/test_flow/test_Authentication/test_login.py
+++ Auto_Test/test_flow/test_Authentication/test_login.py
@@ -60,6 +60,7 @@ def test_companyRegister():
             option.add_argument('--start-maximized')  # 最大化运行（全屏窗口）设置元素定位比较准确
             option.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
             # option.add_argument('--window-size=1920,1080') # 设置浏览器分辨率（窗口大小）
+            sleep(5)
             driver = webdriver.Chrome(options=option)
         else:
             driver = webdriver.Chrome(executable_path=driverPath)
