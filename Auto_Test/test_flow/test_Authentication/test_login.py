c2e7e338f20b6d297e7be547ef077272e8be95c2
unknown <shaojunshuai@chengtay.com>
2020-11-24 16:55:26 +0800

    'add'

--- Auto_Test/test_flow/test_Authentication/test_login.py
+++ Auto_Test/test_flow/test_Authentication/test_login.py
@@ -55,12 +55,12 @@ def test_companyRegister():
 
         if jenkins:
             option = webdriver.ChromeOptions()
-            option.add_argument('--headless')  # 浏览器不提供可视化页面
-            option.add_argument('--no-sandbox')  # 以最高权限运行
+            option.add_argument('headless')  # 浏览器不提供可视化页面
+            option.add_argument('no-sandbox')  # 以最高权限运行
             option.add_argument('--start-maximized')  # 最大化运行（全屏窗口）设置元素定位比较准确
             option.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
-            option.add_argument('--disable-dev-shm-usage')  # 不加载图片, 提升速度
-            driver = webdriver.Chrome(options =option)
+            # option.add_argument('--window-size=1920,1080') # 设置浏览器分辨率（窗口大小）
+            driver = webdriver.Chrome(options=option)
         else:
             driver = webdriver.Chrome(executable_path=driverPath)
         driver.maximize_window()
